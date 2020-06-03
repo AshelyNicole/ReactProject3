9de95d8ceaead873bfdd3873060c1cab1527bfbe
Ashely <arumbaoa10@gmail.com>
2020-06-02 20:58:43 -0700

    Initial routes and js files

--- project03/src/App.js
+++ project03/src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './Home';
+import Dashboard from './Dashboard';
+import Games from './games/Games';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+        <Router>
+          <Switch>
+            <Route exact path="/" component= {Home} />
+            <Route exact path="/dashboard" component= {Dashboard} />
+            <Route exact path="/games" component= {Games} />
+          </Switch>
+        </Router>
+    </React.Fragment>
   );
 }
 
