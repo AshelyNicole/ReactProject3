1b80a27fcb1654f5c719088f7f38972570c2ce36
jmorales113 <jmorales113@gmail.com>
2020-06-07 13:01:25 -0700

    Added NavBar

---
+++ project03/src/components/Dashboard.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./styles/Dashboard.css";
+
+function Dashboard() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+function NavBar() {
+  return (
+    <div className="fixed-top">
+      <div className="collapse" id="navbarToggleExternalContent">
+        <div className="bg-secondary p-4">
+          <h5 className="text-white h4">Collapsed content</h5>
+          <span className="text-muted">Toggleable via the navbar brand.</span>
+        </div>
+      </div>
+      <nav className="navbar navbar-dark bg-secondary">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarToggleExternalContent"
+          aria-controls="navbarToggleExternalContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+      </nav>
+    </div>
+  );
+}
+
+export default Dashboard;
