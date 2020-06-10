14a610d087f51df0760cc6d9a95dee02d9ccff20
jmorales113 <jmorales113@gmail.com>
2020-06-10 12:34:28 -0700

    Set up routes and are rendering

--- project03/src/App.js
+++ project03/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/games/Games";
+import Join from "./components/chat/Join";
+import Chat from "./components/chat/Chat";
 
 class App extends Component {
   render() {
@@ -11,6 +13,8 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={Login} />
             <Route path="/home" component={Dashboard} />
+            <Route path="/join" exact component={Join} />
+            <Route path="/chat" exact component={Chat} />
           </Switch>
         </Router>
       </React.Fragment>
