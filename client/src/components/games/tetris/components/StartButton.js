83859bdce25e910e5cd83fce9fb170c7aa3d9a4f
AshelyNicole <55964517+AshelyNicole@users.noreply.github.com>
2020-06-16 21:10:50 -0700

    Revert "Trial"

--- client/src/components/games/tetris/components/StartButton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {StyledStartButton} from './styles/StyledStartButton'
-
-const StartButton = ({ callback }) => (
-  <StyledStartButton onClick={callback}>Start Game</StyledStartButton>
-);
-
-export default StartButton;
