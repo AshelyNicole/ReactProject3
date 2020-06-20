3de914dc12e72dee4e7f12ff63bed6e6aff104d5
Ashely <arumbaoa10@gmail.com>
2020-06-20 08:48:39 -0700

    testing commenting out env_prod

--- server.js
+++ server.js
@@ -93,13 +93,13 @@ app.use("/api/users", users);
 
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
+//if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client","build", "index.html"));
   })
-}
+//}
 
 
 
